8775a961829900890b9073c7ef128c4045eeca2b
me7alix <me7alix@gmail.com>
2025-07-29 20:23:10 +0300

    changed templates file extension to avoid nvim tree-sitter bugs

--- demo/tmpls.c
+++ demo/tmpls.c
@@ -3,7 +3,7 @@
 
 int main() {
 	StringBuilder tb = tmpls_builder_create();
-	tmpls_builder_compile_template(&tb, "./demo/templates/index.tmpl");
+	tmpls_builder_compile_template(&tb, "./demo/templates/index.htmpl");
 	tmpls_builder_write(&tb, "./build/tmpls.c");
 	tmpls_builder_destroy(&tb);
 	return 0;
