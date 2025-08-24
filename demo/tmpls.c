16d1996b3ceff54e15e6f4f779739d5c6828c363
me7alix <me7alix@gmail.com>
2025-08-24 17:34:11 +0300

    added prefix to sb

--- demo/tmpls.c
+++ demo/tmpls.c
@@ -2,7 +2,7 @@
 #include "../htmpl.h"
 
 int main() {
-	StringBuilder tb = tmpls_builder_create();
+	HTMPL_StringBuilder tb = tmpls_builder_create();
 	tmpls_builder_compile_template(&tb, "./demo/templates/index.htmpl");
 	tmpls_builder_write(&tb, "./build/tmpls.c");
 	tmpls_builder_destroy(&tb);
--- htmpl.h
+++ htmpl.h
@@ -11,33 +11,33 @@
 #define tmpl_load(lib, name) ((name) = dlsym(lib, #name))
 #define HTML(format, ...) { \
 	size_t len = snprintf(NULL, 0, format, ##__VA_ARGS__); \
-	sb_ensure_capacity(&buf, len); \
+	htmpl_sb_ensure_capacity(&buf, len); \
 	sprintf(buf.str, format, ##__VA_ARGS__); \
-	sb_append_str(&l_html, buf.str); \
+	htmpl_sb_append_str(&l_html, buf.str); \
 }
 
 typedef struct {
 	char   *str;
 	size_t	cnt;
 	size_t	cap;
-} StringBuilder;
+} HTMPL_StringBuilder;
 
-StringBuilder sb_create(size_t cap);
-void sb_ensure_capacity(StringBuilder *sb, size_t extra);
-void sb_append_str(StringBuilder *sb, const char *s);
-void sb_append_char(StringBuilder *sb, char ch);
-void sb_reset(StringBuilder *sb);
-void sb_destroy(StringBuilder *sb);
+HTMPL_StringBuilder htmpl_sb_create(size_t cap);
+void htmpl_sb_ensure_capacity(HTMPL_StringBuilder *sb, size_t extra);
+void htmpl_sb_append_str(HTMPL_StringBuilder *sb, const char *s);
+void htmpl_sb_append_char(HTMPL_StringBuilder *sb, char ch);
+void htmpl_sb_reset(HTMPL_StringBuilder *sb);
+void htmpl_sb_destroy(HTMPL_StringBuilder *sb);
 
 char *file_read(const char *filepath);
 void file_write(const char *filepath, char *str);
 
 void *tmpls_load(const char *libfile);
-StringBuilder tmpls_builder_create();
-void tmpls_builder_write(StringBuilder *sb, const char *filepath);
-void tmpls_builder_destroy(StringBuilder *sb);
+HTMPL_StringBuilder tmpls_builder_create();
+void tmpls_builder_write(HTMPL_StringBuilder *sb, const char *filepath);
+void tmpls_builder_destroy(HTMPL_StringBuilder *sb);
 void tmpls_builder_compile_template(
-		StringBuilder *tmpls_builder,
+		HTMPL_StringBuilder *tmpls_builder,
 		const char *input_file
 		);
 
@@ -45,8 +45,8 @@ void tmpls_builder_compile_template(
 
 #ifdef HTMPL_IMPLEMENTATION
 
-StringBuilder sb_create(size_t cap) {
-	StringBuilder sb;
+HTMPL_StringBuilder htmpl_sb_create(size_t cap) {
+	HTMPL_StringBuilder sb;
 	sb.str = (char *) malloc(cap);
 	sb.cnt = 0;
 	sb.cap = cap;
@@ -54,7 +54,7 @@ StringBuilder sb_create(size_t cap) {
 	return sb;
 }
 
-void sb_ensure_capacity(StringBuilder *sb, size_t extra) {
+void htmpl_sb_ensure_capacity(HTMPL_StringBuilder *sb, size_t extra) {
 	size_t required = sb->cnt + extra + 1;
 	if (required <= sb->cap) return;
 	while (sb->cap < required) {
@@ -63,26 +63,26 @@ void sb_ensure_capacity(StringBuilder *sb, size_t extra) {
 	sb->str = (char *) realloc(sb->str, sb->cap);
 }
 
-void sb_append_str(StringBuilder *sb, const char *s) {
+void htmpl_sb_append_str(HTMPL_StringBuilder *sb, const char *s) {
 	size_t len = strlen(s);
-	sb_ensure_capacity(sb, len);
+	htmpl_sb_ensure_capacity(sb, len);
 	memcpy(sb->str + sb->cnt, s, len);
 	sb->cnt += len;
 	sb->str[sb->cnt] = '\0';
 }
 
-void sb_append_char(StringBuilder *sb, char ch) {
-	sb_ensure_capacity(sb, 1);
+void htmpl_sb_append_char(HTMPL_StringBuilder *sb, char ch) {
+	htmpl_sb_ensure_capacity(sb, 1);
 	sb->str[sb->cnt++] = ch;
 	sb->str[sb->cnt] = '\0';
 }
 
-void sb_reset(StringBuilder *sb) {
+void htmpl_sb_reset(HTMPL_StringBuilder *sb) {
 	sb->cnt = 0;
 	if (sb->str) sb->str[0] = '\0';
 }
 
-void sb_destroy(StringBuilder *sb) {
+void htmpl_sb_destroy(HTMPL_StringBuilder *sb) {
 	free(sb->str);
 	sb->str = NULL;
 	sb->cnt = sb->cap = 0;
@@ -119,10 +119,10 @@ void file_write(const char *filepath, char *str) {
 }
 
 void tmpls_builder_compile_template(
-	StringBuilder *tmpls_builder,
+	HTMPL_StringBuilder *tmpls_builder,
 	const char *input_file
 ) {
-	StringBuilder compressed_html = sb_create(128);
+	HTMPL_StringBuilder compressed_html = htmpl_sb_create(128);
 	char *file_str = file_read(input_file);
 	char *file_str_p = file_str;
 	if (file_str == NULL) {
@@ -130,16 +130,16 @@ void tmpls_builder_compile_template(
 		exit(1);
 	}
 
-	StringBuilder tmpl_name = sb_create(16);
-	StringBuilder tmpl_args = sb_create(32);
+	HTMPL_StringBuilder tmpl_name = htmpl_sb_create(16);
+	HTMPL_StringBuilder tmpl_args = htmpl_sb_create(32);
 
 	bool br_fl = false;
 	for (; *file_str != ')'; file_str++) {
 		if (*file_str == '(') br_fl = true;
 		else if (!br_fl) {
-			sb_append_char(&tmpl_name, *file_str);
+			htmpl_sb_append_char(&tmpl_name, *file_str);
 		} else {
-			sb_append_char(&tmpl_args, *file_str);
+			htmpl_sb_append_char(&tmpl_args, *file_str);
 		}
 	}
 
@@ -150,7 +150,7 @@ void tmpls_builder_compile_template(
 		if (!(*file_str == '\t' || *file_str == ' ')) ln_flag = false;
 		if (*file_str == '\n') ln_flag = true;
 		if (!ln_flag) {
-			sb_append_char(&compressed_html, *file_str);
+			htmpl_sb_append_char(&compressed_html, *file_str);
 		}
 	}
 
@@ -158,36 +158,36 @@ void tmpls_builder_compile_template(
 
 	char *tmpl_str = compressed_html.str;
 
-	StringBuilder tmpl = sb_create(128);
+	HTMPL_StringBuilder tmpl = htmpl_sb_create(128);
 
-	sb_append_str(&tmpl, "char *");
-	sb_append_str(&tmpl, tmpl_name.str);
-	sb_append_str(&tmpl, "(");
-	sb_append_str(&tmpl, tmpl_args.str);
-	sb_append_str(&tmpl, ") {");
-	sb_append_str(&tmpl, "StringBuilder buf = sb_create(128),");
-	sb_append_str(&tmpl, "l_html = sb_create(128);");
+	htmpl_sb_append_str(&tmpl, "char *");
+	htmpl_sb_append_str(&tmpl, tmpl_name.str);
+	htmpl_sb_append_str(&tmpl, "(");
+	htmpl_sb_append_str(&tmpl, tmpl_args.str);
+	htmpl_sb_append_str(&tmpl, ") {");
+	htmpl_sb_append_str(&tmpl, "HTMPL_StringBuilder buf = htmpl_sb_create(128),");
+	htmpl_sb_append_str(&tmpl, "l_html = htmpl_sb_create(128);");
 
 	bool c_code = false;
 	bool bquotes = false;
 	int br_cnt = 0;
 
-	StringBuilder code = sb_create(64);
-	StringBuilder html = sb_create(64);
+	HTMPL_StringBuilder code = htmpl_sb_create(64);
+	HTMPL_StringBuilder html = htmpl_sb_create(64);
 
 	while (*tmpl_str != '\0') {
 		if (c_code && *tmpl_str != '\n') {
 			if (*tmpl_str == '\\' && *(tmpl_str + 1) == '`') {
-				sb_append_char(&code, '`');
+				htmpl_sb_append_char(&code, '`');
 				tmpl_str++;
 			} else if (*tmpl_str == '`') {
 				bquotes = !bquotes;
-				sb_append_char(&code, '"');
+				htmpl_sb_append_char(&code, '"');
 			} else {
 				if (bquotes && *tmpl_str == '"') {
-					sb_append_char(&code, '\\');
-					sb_append_char(&code, '"');
-				} else sb_append_char(&code, *tmpl_str);
+					htmpl_sb_append_char(&code, '\\');
+					htmpl_sb_append_char(&code, '"');
+				} else htmpl_sb_append_char(&code, *tmpl_str);
 			}
 		}
 
@@ -196,10 +196,10 @@ void tmpls_builder_compile_template(
 				c_code = true;
 				br_cnt = 0;
 
-				sb_append_str(&tmpl, "sb_append_str(&l_html, \"");
-				sb_append_str(&tmpl, html.str);	
-				sb_append_str(&tmpl, "\");");
-				sb_reset(&html);	
+				htmpl_sb_append_str(&tmpl, "htmpl_sb_append_str(&l_html, \"");
+				htmpl_sb_append_str(&tmpl, html.str);	
+				htmpl_sb_append_str(&tmpl, "\");");
+				htmpl_sb_reset(&html);	
 				break;
 
 			case '{':
@@ -214,75 +214,75 @@ void tmpls_builder_compile_template(
 		if (!c_code) {
 			switch (*tmpl_str) {
 				case '\"': case '\\':
-					sb_append_char(&html, '\\');
-					sb_append_char(&html, *tmpl_str);
+					htmpl_sb_append_char(&html, '\\');
+					htmpl_sb_append_char(&html, *tmpl_str);
 					break;
 
 				case '\n':
 					break;
 
 				default:
-					sb_append_char(&html, *tmpl_str);
+					htmpl_sb_append_char(&html, *tmpl_str);
 					break;
 			}
 		}
 
 		if (c_code && *tmpl_str == '}' && br_cnt == 0) {
 			c_code = false;
-			sb_append_str(&tmpl, code.str);
-			sb_reset(&code);
+			htmpl_sb_append_str(&tmpl, code.str);
+			htmpl_sb_reset(&code);
 		}
 
 		tmpl_str++;
 	}
 
 	if (!c_code) {
-		sb_append_str(&tmpl, "sb_append_str(&l_html, \"");
-		sb_append_str(&tmpl, html.str);	
-		sb_append_str(&tmpl, "\");");
-		sb_reset(&html);
+		htmpl_sb_append_str(&tmpl, "htmpl_sb_append_str(&l_html, \"");
+		htmpl_sb_append_str(&tmpl, html.str);	
+		htmpl_sb_append_str(&tmpl, "\");");
+		htmpl_sb_reset(&html);
 	}
 
-	sb_append_str(&tmpl, "sb_destroy(&buf);");
-	sb_append_str(&tmpl, "return l_html.str;");
-	sb_append_str(&tmpl, "}");
+	htmpl_sb_append_str(&tmpl, "htmpl_sb_destroy(&buf);");
+	htmpl_sb_append_str(&tmpl, "return l_html.str;");
+	htmpl_sb_append_str(&tmpl, "}");
 
-	sb_destroy(&code);
-	sb_destroy(&html);
+	htmpl_sb_destroy(&code);
+	htmpl_sb_destroy(&html);
 
 	//file_write(output_file, tmpl.str);
-	sb_append_str(tmpls_builder, tmpl.str);
-	sb_destroy(&tmpl);
+	htmpl_sb_append_str(tmpls_builder, tmpl.str);
+	htmpl_sb_destroy(&tmpl);
 }
 
-StringBuilder tmpls_builder_create() {
-	StringBuilder tmpls_builder = sb_create(2048);
-	sb_append_str(&tmpls_builder, "#include <stdlib.h>\n");
-	sb_append_str(&tmpls_builder, "#include <stdio.h>\n");
-	sb_append_str(&tmpls_builder, "#include <string.h>\n");
-	sb_append_str(&tmpls_builder, "#include <stdbool.h>\n");
-	sb_append_str(&tmpls_builder, "#include <stdint.h>\n");
-	sb_append_str(&tmpls_builder, "#define HTML(format, ...) { \
+HTMPL_StringBuilder tmpls_builder_create() {
+	HTMPL_StringBuilder tmpls_builder = htmpl_sb_create(2048);
+	htmpl_sb_append_str(&tmpls_builder, "#include <stdlib.h>\n");
+	htmpl_sb_append_str(&tmpls_builder, "#include <stdio.h>\n");
+	htmpl_sb_append_str(&tmpls_builder, "#include <string.h>\n");
+	htmpl_sb_append_str(&tmpls_builder, "#include <stdbool.h>\n");
+	htmpl_sb_append_str(&tmpls_builder, "#include <stdint.h>\n");
+	htmpl_sb_append_str(&tmpls_builder, "#define HTML(format, ...) { \
 			size_t len = snprintf(NULL, 0, format, ##__VA_ARGS__); \
-			sb_ensure_capacity(&buf, len); \
+			htmpl_sb_ensure_capacity(&buf, len); \
 			sprintf(buf.str, format, ##__VA_ARGS__); \
-			sb_append_str(&l_html, buf.str); \
+			htmpl_sb_append_str(&l_html, buf.str); \
 			}\n");
-sb_append_str(&tmpls_builder, "typedef struct {char *str;size_t cnt, cap;} StringBuilder;");
-sb_append_str(&tmpls_builder, "StringBuilder sb_create(size_t cap) {StringBuilder sb;sb.str = (char *) malloc(cap);sb.cnt = 0;sb.cap = cap;if (sb.str) sb.str[0] = '\\0';return sb;}");
-sb_append_str(&tmpls_builder, "void sb_ensure_capacity(StringBuilder *sb, size_t extra) {size_t required = sb->cnt + extra + 1;if (required <= sb->cap) return;while (sb->cap < required) {sb->cap *= 2;}sb->str = (char *) realloc(sb->str, sb->cap);}");
-sb_append_str(&tmpls_builder, "void sb_append_str(StringBuilder *sb, const char *s) {size_t len = strlen(s);sb_ensure_capacity(sb, len);memcpy(sb->str + sb->cnt, s, len);sb->cnt += len;sb->str[sb->cnt] = '\\0';}");
-sb_append_str(&tmpls_builder, "void sb_reset(StringBuilder *sb) {sb->cnt = 0;if (sb->str) sb->str[0] = '\\0';}");
-sb_append_str(&tmpls_builder, "void sb_destroy(StringBuilder *sb) {free(sb->str);sb->str = NULL;sb->cnt = sb->cap = 0;}");
+htmpl_sb_append_str(&tmpls_builder, "typedef struct {char *str;size_t cnt, cap;} HTMPL_StringBuilder;");
+htmpl_sb_append_str(&tmpls_builder, "HTMPL_StringBuilder htmpl_sb_create(size_t cap) {HTMPL_StringBuilder sb;sb.str = (char *) malloc(cap);sb.cnt = 0;sb.cap = cap;if (sb.str) sb.str[0] = '\\0';return sb;}");
+htmpl_sb_append_str(&tmpls_builder, "void htmpl_sb_ensure_capacity(HTMPL_StringBuilder *sb, size_t extra) {size_t required = sb->cnt + extra + 1;if (required <= sb->cap) return;while (sb->cap < required) {sb->cap *= 2;}sb->str = (char *) realloc(sb->str, sb->cap);}");
+htmpl_sb_append_str(&tmpls_builder, "void htmpl_sb_append_str(HTMPL_StringBuilder *sb, const char *s) {size_t len = strlen(s);htmpl_sb_ensure_capacity(sb, len);memcpy(sb->str + sb->cnt, s, len);sb->cnt += len;sb->str[sb->cnt] = '\\0';}");
+htmpl_sb_append_str(&tmpls_builder, "void htmpl_sb_reset(HTMPL_StringBuilder *sb) {sb->cnt = 0;if (sb->str) sb->str[0] = '\\0';}");
+htmpl_sb_append_str(&tmpls_builder, "void htmpl_sb_destroy(HTMPL_StringBuilder *sb) {free(sb->str);sb->str = NULL;sb->cnt = sb->cap = 0;}");
 return tmpls_builder;
 }
 
-void tmpls_builder_write(StringBuilder *sb, const char *filepath) {
+void tmpls_builder_write(HTMPL_StringBuilder *sb, const char *filepath) {
 	file_write(filepath, sb->str);
 }
 
-void tmpls_builder_destroy(StringBuilder *sb) {
-	sb_destroy(sb);
+void tmpls_builder_destroy(HTMPL_StringBuilder *sb) {
+	htmpl_sb_destroy(sb);
 }
 
 void *tmpls_load(const char *libfile) {
